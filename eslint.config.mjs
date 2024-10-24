bd4f335067afe213f12dd5ebcc29510cdd150350
KolfinnaK <kolfinnakolbeinsdottir@gmail.com>
2024-10-24 23:38:28 +0000

    build dot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,5 @@ import pluginJs from "@eslint/js";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
